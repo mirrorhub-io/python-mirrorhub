18b61c7ee8e1eb7431313ce47273f547b2b616a6
Markus Freitag <markus.freitag@hetzner.de>
2017-01-27 18:33:11 +0100

    add basic setup file
    Signed-off-by: Markus Freitag <fmarkus@mailbox.org>

---
+++ setup.py
@@ -0,0 +1,11 @@
+'''setup file to install the mirrorhub module'''
+from setuptools import setup
+
+setup(name='mirrorhub',
+      version='0.1.0',
+      description='Module to interact with Mirrorhub.io',
+      author='Markus Freitag',
+      author_email='fmarkus@mailbox.org',
+      license='MIT',
+      packages=['mirrorhub'],
+      zip_safe=False)
