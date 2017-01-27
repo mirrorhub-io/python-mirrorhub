7159f2755a02ddefd80a8a105ee4a7146dd0df8f
Markus Freitag <markus.freitag@hetzner.de>
2017-01-27 18:42:21 +0100

    define constants and path for configuration/template files
    Signed-off-by: Markus Freitag <fmarkus@mailbox.org>

--- mirrorhub/container.py
+++ mirrorhub/container.py
@@ -1,1 +1,20 @@
 '''submodule if mirrorhub, which provides method to initialize the mirror container'''
+import os
+import signal
+import socket
+from subprocess import Popen
+from jinja2 import Template
+
+from mirrorhub.utils import exec_cmd
+
+HOSTNAME = socket.gethostname()
+LETSENCRYPT_ARGS = ' '.join(['-a webroot',
+                             '--webroot-path=/tmp/letsencrypt',
+                             '--rsa-key-size 4096',
+                             '--non-interactive'])
+PATHS = {'letsencrypt': '',
+         'dhparams': '/srv/nginx/dhparam.pem',
+         'templates': '/srv/internals/',
+         'rsync': '/etc/rsyncd.conf',
+         'nginx': {'s-a': '/etc/nginx/sites-available/mirror.conf',
+                   's-e': '/etc/nginx/sites-enabled/mirror.conf'}}
