bfc160c5d4ca4424c8a1ad0c4d0c25ceedda24aa
Markus Freitag <markus.freitag@hetzner.de>
2017-01-27 19:38:30 +0100

    introduce APIClient class
    Signed-off-by: Markus Freitag <markus.freitag@hetzner.de>

--- mirrorhub/api.py
+++ mirrorhub/api.py
@@ -1,1 +1,7 @@
 '''submodule of mirrorhub, which provides an api client'''
+import requests as req
+
+class APIClient():
+    def __init__(self, token):
+        self.token = token
+        self.session = None
