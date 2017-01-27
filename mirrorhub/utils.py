bf768d0ad20f6cd79999025c8e290fd1bac914b5
Markus Freitag <markus.freitag@hetzner.de>
2017-01-27 18:38:58 +0100

    add wrapper method for exec shell commands
    Signed-off-by: Markus Freitag <fmarkus@mailbox.org>

--- mirrorhub/utils.py
+++ mirrorhub/utils.py
@@ -1,1 +1,8 @@
 '''submodules which provides useful helper methods'''
+from subprocess import run, DEVNULL, STDOUT
+
+def exec_cmd(command):
+    '''execute a shell command
+    args:
+        command (str): shell command'''
+    run(command, stdout=DEVNULL, stderr=STDOUT, shell=True)
