40c2b315d7b3ff43495d2be98e1b2ff0c85fba7c
Markus Freitag <markus.freitag@hetzner.de>
2017-01-30 18:30:29 +0100

    add method to retrieve mirror details via the API
    Signed-off-by: Markus Freitag <markus.freitag@hetzner.de>

--- mirrorhub/api.py
+++ mirrorhub/api.py
@@ -2,12 +2,27 @@
 
 which provides an api client
 """
+import requests as req
 
 
 class APIClient():
     """Client class for API interaction."""
 
+    URL = 'https://mirrorhub.io'
+
     def __init__(self, token):
         """Initialize a new APIClient."""
         self.token = token
-        self.session = None
+        self.header = {'Grpc-Metadata-ClientToken': token}
+
+    def _get(self, url):
+        """Wrapper method for HTTP GET requests."""
+        return req.get(self.URL + url, headers=self.header)
+
+    def get_mirror_info(self):
+        """Get the mirror details from the API.
+
+        Returns:
+            dict: data as JSON
+        """
+        return self._get('/v1/mirror/self')
