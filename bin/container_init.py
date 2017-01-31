8a0346b6ac30bb4b06c0082265b2e86647049e2d
Markus Freitag <markus.freitag@hetzner.de>
2017-01-31 19:14:11 +0100

    fetch domain and service from api
    Signed-off-by: Markus Freitag <markus.freitag@hetzner.de>

--- bin/container_init.py
+++ bin/container_init.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 """Init script which prepares our docker container."""
+import os
 from mirrorhub import LOGO
 from mirrorhub import container as ct
+from mirrorhub.api import APIClient
 
+api_client = APIClient(os.getenv('CLIENT_TOKEN'))
+data = api_client.get_mirror_info()
+DOMAIN = data['domain']
+SERVICE = data['service']['name']
 
 print(LOGO)
 
@@ -14,21 +20,19 @@ else:
     ct.create_dhparams()
     print('[nginx] Finished generating.')
 
-print('[nginx] Using %s as HOSTNAME' % ct.HOSTNAME)
-
 if ct.sslcert_exists():
     print('[cert] Found certificate for domain. Attempt renew..')
     ct.renew_sslcert()
 else:
     print('[cert] Missing certificate for domain. Request new one..')
-    ct.create_sslcert()
+    ct.create_sslcert(DOMAIN)
 
 
 print('[nginx] Applying final site configuration..')
-ct.build_nginx_conf('mirror')
+ct.build_nginx_conf('mirror', DOMAIN, SERVICE)
 
 print('[rsync] Building rsync configuration..')
-ct.build_rsync_conf()
+ct.build_rsync_conf(SERVICE)
 
 print('[mirror] Ready to serve!')
 ct.run_supervisor()
